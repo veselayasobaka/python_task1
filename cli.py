25b9895fd6917d27823a236a86bb3840cdb7dfba
veselayasobaka <a.marin30692@gmail.com>
2018-07-14 19:30:35 +0300

    added simplifier

--- cli.py
+++ cli.py
@@ -2,16 +2,20 @@
 from differentiator import derive
 print("The program differentiates the entered mathematical " 
       "expression in the x variable and supports the use of " 
-      "variables x, y, z, operators '+', '-', '*', '/', '^'" 
+      "variables x, y, z, operators '+', '-', '*', '/', '^' '(', ')'" 
       "and functions sin(), cos(), tan(), asin(), acos(), "
       "atan(), ln(), exp(), sqrt()")
 print("Input mathematical expression:")
-try:
-    mathexpr = input()
-    answer = derive(mathexpr)
-except ZeroDivisionError:
-    print("Error! Zero division detected.")
-except Exception as err:
-    print(str(err)+"Unkown error")
-else:
-    print("Derivative of this expression:\n"+answer)
+while(True):
+    try:
+        mathexpr = input()
+        if mathexpr == "":break
+        answer = derive(mathexpr)
+    except ZeroDivisionError:
+        print("Error! Zero division detected.")
+    except Exception as err:
+        print(str(err)+"Unkown error. Check the input line.")
+    else:
+        print(answer)
+
+
